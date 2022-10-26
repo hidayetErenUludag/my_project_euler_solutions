6ea85ee637cf687656f3ffe327832f81295ecee7
mightymountain <h.eren.uludag@gmail.com>
2022-10-26 15:55:33 +0300

    fix

--- palindromic.py
+++ palindromic.py
@@ -1,1 +1,11 @@
-
+def ispalindrome(n):
+    return str(n) == str(n)[::-1]
+liste = []
+for a in range(100, 1000):
+    for b in range(100, 1000):
+        c = a*b
+        if ispalindrome(c):
+            liste.append(c)
+clean_liste = [*set(liste)]
+clean_liste.sort(reverse=True)
+print(clean_liste)
