e31ce1a32bac3e213e603b3df4cd1e91aa8ce047
mightymountain <h.eren.uludag@gmail.com>
2022-10-27 10:02:33 +0300

    fix

--- sumsqudiff.py
+++ sumsqudiff.py
@@ -1,1 +1,19 @@
+i = 1
+num_list = [] #stores the i squared
+while i < 101:
+    num_list.append(i*i) # finds the spare of i
+    i = i+1 # adds 1 to i
 
+
+a = 1
+num_lisit_2 = []
+
+while a < 101:
+    num_lisit_2.append(a)
+    a = a+1
+
+
+s = sum(num_lisit_2)
+c = sum(num_list)
+
+print((s**2)-c)
