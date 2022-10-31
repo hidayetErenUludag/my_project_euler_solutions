aed3684cbc618d6c8447d496b8e12322bf4791f7
mightymountain <h.eren.uludag@gmail.com>
2022-10-31 13:55:06 +0300

    fix

--- Pythagorean _triple.py
+++ Pythagorean _triple.py
@@ -1,1 +1,8 @@
-
+def euler():
+    for c in range(2, 1000):
+        for a in range(1, c):
+            b = 1000 - c - a
+            if a**2 + b**2 == c**2:
+                print(a, b, c, a * b * c)
+                return
+print(euler())
