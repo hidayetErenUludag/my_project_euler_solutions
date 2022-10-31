5462d33aa9e05091e9da25e38cfb25a4299dc014
mightymountain <h.eren.uludag@gmail.com>
2022-10-31 15:51:03 +0300

    fix

--- sum_primes_below_2m.py
+++ sum_primes_below_2m.py
@@ -1,1 +1,23 @@
 
+def primes_upto(limit):
+    limitN = limit+1
+    not_prime = set()
+    primes = [2]
+
+    for i in range(3, limitN, 2):
+        if i in not_prime:
+            continue
+
+        for j in range(i*3, limitN, i*2):
+            not_prime.add(j)
+
+        primes.append(i)
+    return primes
+
+print(sum(primes_upto(2000000)))
+
+
+
+
+
+
