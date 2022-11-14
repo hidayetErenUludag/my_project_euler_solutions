0d33d5930855dba129532e7dc7e83d3a50d07d87
mightymountain <h.eren.uludag@gmail.com>
2022-11-14 12:22:42 +0300

    fix

--- high_div_nums.py
+++ high_div_nums.py
@@ -1,1 +1,18 @@
+def tri_num(n):
+    num = ((n+1)*n)//2
+    return num
 
+
+def high_div(m):
+    count = 1
+    a = tri_num(m)
+    for j in range(1, a):
+        if a % j == 0:
+            count += 1
+        else:
+            count = count
+    print(count)
+    print(tri_num(m))
+
+
+print(tri_num(12375))
---
+++ test.py
@@ -0,0 +1,5 @@
+def tri_num(n):
+    num = ((n+1)*n)//2
+    return num
+
+print(tri_num(8))
