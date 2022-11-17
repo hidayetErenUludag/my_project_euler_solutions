06b49ef2240a3dce70c0fd5af39a0dc8611b43bd
mightymountain <h.eren.uludag@gmail.com>
2022-11-17 20:21:20 +0300

    fix

--- lattice_path.py
+++ lattice_path.py
@@ -1,1 +1,12 @@
+def way_finder(n):
+    a = int(input("first grid :"))
+    b = int(input("second grid :"))
+    c = 1
+    d = 1
+    for i in range(1, a+b+1):
+        c *= i
+    for j in range(1, b+1):
+        d *= j
+    print(c/(d*d))
 
+way_finder(1)
--- test.py
+++ test.py
@@ -1,26 +1,12 @@
+def way_finder(n):
+    a = int(input("first grid :"))
+    b = int(input("second grid :"))
+    c = 1
+    d = 1
+    for i in range(1, a+b+1):
+        c *= i
+    for j in range(1, b+1):
+        d *= j
+    print(c/(d*d))
 
-def collatz(i):
-    count = 0
-    while i > 1:
-        if i % 2 == 0:
-            i = i/2
-            count += 1
-        else:
-            i = 3*i + 1
-            count += 1
-    return count
-
-
-def sorter(n):
-    count_global = 0
-    if count_global < collatz(n):
-        count_global = collatz(n)
-        num_global = n
-    else:
-        count_global = count_global
-        num_global = n
-    return (count_global, num_global)
-
-
-for i in range(1, 1000001):
-    print(sorter(i))
+way_finder(1)
