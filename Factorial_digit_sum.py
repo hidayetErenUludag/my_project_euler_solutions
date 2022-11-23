497812695cdedd4ef03c92da2d6b26053be35d23
mightymountain <h.eren.uludag@gmail.com>
2022-11-23 13:57:59 +0300

    fix

--- Factorial_digit_sum.py
+++ Factorial_digit_sum.py
@@ -1,1 +1,12 @@
 
+def factorial_digit_sum(n):
+    b = 1
+    c = 0
+    for i in range(1, n+1):
+        b = i*b
+    a = [int(x) for x in str(b)]
+    print(sum(a))
+
+
+factorial_digit_sum(100)
+
