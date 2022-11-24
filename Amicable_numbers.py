52a0e718f4a06b66230791921178270d38516d97
mightymountain <h.eren.uludag@gmail.com>
2022-11-24 12:03:38 +0300

    fix

--- Amicable_numbers.py
+++ Amicable_numbers.py
@@ -1,1 +1,23 @@
+def divers(n):
+    num1 = 0
+    for i in range(1, n):
+        if n % i == 0:
+            num1 += i
+    return num1
 
+
+def comper(n):
+    a = divers(n)
+    b = divers(a)
+    if b == n and b != a:
+        return n
+    else:
+        return None
+
+
+list = []
+b = 0
+for j in range(1, 10001):
+    if comper(j):
+        list.append(comper(j))
+print(sum(list))
