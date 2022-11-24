ad99ffaffbcb9a6f8aed922734d2d60041f50e7c
mightymountain <h.eren.uludag@gmail.com>
2022-11-24 13:41:53 +0300

    fix

--- Names_scores.py
+++ Names_scores.py
@@ -1,1 +1,22 @@
+f = open('/Users/hidayeterenuludag/Desktop/p022_names.txt')
+string = f.readlines()
+f.close()
+names = sorted(str(string).split(","))
+print(names[1])
+def value_name(name):
+    alphet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    value = 0
+    for i in name:
+        if i in alphet:
+            value += alphet.index(i) + 1
+    return value
 
+# from https://stackoverflow.com/questions/50982483/names-scores-project-euler-22
+
+with open("/Users/hidayeterenuludag/Desktop/p022_names.txt", "r") as File:
+    Data = sorted(File.readlines()[0].split(','))
+
+    result = 0
+    for position, name in enumerate(Data):
+        name_score = value_name(name) * (position + 1)
+        result += name_score
---
+++ wefg.py
@@ -0,0 +1,5 @@
+values ={A}
+def order():
+    splitted = open('/Users/hidayeterenuludag/Desktop/p022_names.txt').read().replace('","', ',').replace('"', '').split(',')
+
+
