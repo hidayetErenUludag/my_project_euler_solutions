e3f88f279b18e673b25f9a19b35a1e2497806a48
mightymountain <h.eren.uludag@gmail.com>
2022-11-29 11:03:07 +0300

    fix

--- 1000_digit_fibonacci.py
+++ 1000_digit_fibonacci.py
@@ -1,1 +1,13 @@
+def fib_1000():
+    a, b = 1, 1
+    count = 2
+    while count < 5000:
+        count += 1
+        a, b = b, a + b
+        if len(str(b)) >= 1000:
+            print(b)
+            print(count)
+            return
 
+
+fib_1000()
