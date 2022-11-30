7c887829e0bfc5264fa0b8b9777308a9488f5cb8
mightymountain <h.eren.uludag@gmail.com>
2022-11-30 11:54:03 +0300

    fix

--- Reciprocal_cycles.py
+++ Reciprocal_cycles.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
 
+
+def reciprocal(n):
+    for i in range(2, n):
+        if 1 == (10**i % n):
+            return i
+    return 0
+
+
+a = []
+for j in range(2,1001):
+    a.append(reciprocal(j))
+plt.plot(a)
+
+
+long = [max(reciprocal(i) for i in range(2, 1001))]
+print([i for i in range(2, 1001) if reciprocal(i) == long[0]])
+
+plt.show()
