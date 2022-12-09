b21f1be9cf1af5db541e5cedfcdee2fbdb3fa309
mightymountain <h.eren.uludag@gmail.com>
2022-12-09 14:13:01 +0300

    new question

--- Number_spiral_diagonals.py
+++ Number_spiral_diagonals.py
@@ -1,1 +1,22 @@
+import time
+
+start = time.time()
+def calc():
+    count = 1
+    current_num = 1
+    for square in range(1, 501):
+        a = 0
+        b = 0
+        b += current_num + square*2
+        a += (b + 3*(square*2))
+        current_num = a
+        count += (a+b)*2
+        square += 1
+    return count
+
+a = calc()
+end = time.time()
+print(a, (end-start))
+
+
 
--- test.py
+++ test.py
@@ -1,53 +1,23 @@
-# Function to check if a given number is prime
+def clac():
+  count = 1  # Initialize the running total of the sum to 1 for the center point
+  current_num = 1
+  square = 1
+  while square <= 2:
+    # Compute the corner points of the square using simple arithmetic
+    a = current_num + square * 2
+    b = a + square * 2
+    c = b + square * 2
+    d = c + square * 2
 
-import time
-start_time = time.time()
+    # Add the corner points to the running total of the sum
+    count += a + b + c + d
 
+    # Move to the next square and update the current number
+    current_num = d
+    square += 2
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+  # Print the result
+  print(count)
 
-
-def evaluate_quadratic(n, a, b):
-    return n ** 2 + a * n + b
-
-
-# Function to find the maximum number of consecutive primes for the given range of a and b
-def find_max_consecutive_primes(a_range, b_range):
-    max_consecutive_primes = 0
-    max_a = 0
-    max_b = 0
-
-    # Try all possible values of a and b within the given ranges
-    for a in a_range:
-        for b in b_range:
-            consecutive_primes = 0
-            # Evaluate the quadratic expression for n = 0, 1, 2, ...
-            # until we get a non-prime number
-            n = 0
-            while is_prime(evaluate_quadratic(n, a, b)):
-                consecutive_primes += 1
-                n += 1
-            # Update the maximum number of consecutive primes and the corresponding values of a and b
-            if consecutive_primes > max_consecutive_primes:
-                max_consecutive_primes = consecutive_primes
-                max_a = a
-                max_b = b
-
-    # Return the product of the coefficients that produce the maximum number of consecutive primes
-    return max_a * max_b
-
-
-# Find the product of the coefficients for the quadratic expression that produces the maximum number of primes
-# for consecutive values of n, starting with n = 0
-a_range = range(-1000, 1001)
-b_range = range(-1000, 1001)
-print(find_max_consecutive_primes(a_range, b_range))
-end = time.time()
-
-print("--- %s seconds ---" % (time.time() - start_time))
+# Test the function
+clac()
